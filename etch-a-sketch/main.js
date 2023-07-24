283dc86fa6797424fa70b19715c291010c089e8d
doanvalerie <valeriephuongdoan@gmail.com>
2023-07-23 17:55:36 -0700

    Modularize code with classes

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -1,114 +1,210 @@
-setup();
+class Grid {
+	constructor(dimension) {
+		this.dimension = dimension;
+		this.cellsArray = [];
+		this.createGrid();
+	}
 
-function setup() {
-	const gridConfig = {
-		dimension: 16,
-		color: "rgb(0, 0, 0)"
-	};
+	createGrid() {
+		const grid = document.querySelector(".grid");
+		const gridStyle = getComputedStyle(grid);
+		const width = gridStyle.width.replace(/\D/g, "");
+		const pixels = width / this.dimension;
 
-	createGrid(gridConfig);
-	setupEventListeners(gridConfig);
-	setupScaleListener(gridConfig);
-}
-
-function createGrid(gridConfig) {
-	const grid = document.querySelector(".grid");
-	const gridStyle = getComputedStyle(grid);
-	const width = +gridStyle.width.replace(/\D/g, '');
-	const pixels = width / gridConfig.dimension;
-
-	for (let i = 0; i < gridConfig.dimension; ++i) {
-		const row = createGridRow();
-		for (let j = 0; j < gridConfig.dimension; ++j) {
-			const cell = createGridCell(pixels);
-			row.appendChild(cell);
+		for (let row = 0; row < this.dimension; ++row) {
+			const gridRow = this.createGridRow();
+			for (let col = 0; col < this.dimension; ++col) {
+				const cell = new Cell(pixels, "rgb(255, 255, 255)");
+				this.cellsArray.push(cell);
+				gridRow.appendChild(cell.element); 
+			}
+			grid.appendChild(gridRow);
 		}
-		grid.appendChild(row);
-	}
-}
-
-function createGridRow() {
-	const row = document.createElement("div");
-	row.classList.toggle("grid-row");
-	return row;
-}
-
-function createGridCell(pixels) {
-	const cell = document.createElement("div");
-	cell.style.width = `${pixels}px`;
-	cell.style.height = `${pixels}px`;
-	cell.style.background = "rgb(255, 255, 255)";
-	cell.classList.toggle("grid-cell");
-	return cell;
-}
-
-function setupEventListeners(gridConfig) {
-	const colorSelector = document.querySelector(".color-selector");
-	const rainbowSelector = document.querySelector(".rainbow-selector");
-	const eraserSelector = document.querySelector(".eraser-selector");
-	const colorDisplay = document.querySelector("#color-display");
-	const eraserDisplay = document.querySelector("#eraser-display");
-
-	function setCellPaint(e) {
-		e.target.style.backgroundColor = gridConfig.color;
 	}
 
-	function setRandomCellPaint() {
-		gridConfig.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+	createGridRow() {
+		const gridRow = document.createElement("div");
+		gridRow.classList.toggle("grid-row");
+		return gridRow;
 	}
 
-	function handleColorSelector() {
-		const cells = document.querySelectorAll(".grid-cell");
-	
-		colorDisplay.click();
-		colorDisplay.addEventListener("change", () => {
-			gridConfig.color = colorDisplay.value;
-			cells.forEach(cell => {
-				cell.removeEventListener("mouseover", setRandomCellPaint);
-				cell.addEventListener("mouseover", setCellPaint);
-			});
+	resetGrid() {
+		const grid = document.querySelector(".grid");
+
+		while (grid.firstChild) {
+			grid.removeChild(grid.firstChild);
+		}
+
+		this.cellsArray.forEach(cell => {
+			cell.element.removeColorModes();
 		})
 	}
+}
 
-	function handleRainbowSelector() {
-		const cells = document.querySelectorAll(".grid-cell");
-
-		cells.forEach(cell => {
-			cell.addEventListener("mouseover", setCellPaint);
-			cell.addEventListener("mouseover", setRandomCellPaint)	
-		});
+class Cell {
+	constructor(pixels, color) {
+		this.pixels = pixels;
+		this.color = color;
+		this.element = this.createGridCell();
+		this.setSelectCellPaint = this.setSelectCellPaint.bind(this);
+		this.setRandomCellPaint = this.setRandomCellPaint.bind(this);
+		this.eraseCellPaint = this.eraseCellPaint.bind(this);
+		this.setLightShader = this.setLightShader.bind(this);
+		this.setDarkShader = this.setDarkShader.bind(this);
 	}
 
-	function handleEraserSelector() {
-		const cells = document.querySelectorAll(".grid-cell");
+	removeColorModes() {
+		this.element.removeEventListener("mouseover", this.setSelectCellPaint);
+		this.element.removeEventListener("mouseover", this.setRandomCellPaint);
+		this.element.removeEventListener("click", this.eraseCellPaint);
+		this.element.removeEventListener("mouseover", this.setDarkShader);
+		this.element.removeEventListener("mouseover", this.setLightShader);
+	}
 
-		eraserDisplay.click();
-		cells.forEach(cell => {
-			cell.removeEventListener("mouseover", setCellPaint);
-			cell.removeEventListener("mouseover", setRandomCellPaint);
-			cell.addEventListener("click", () => { 
-				cell.style.backgroundColor = eraserDisplay.value;
+	createGridCell() {
+		const cell = document.createElement("div");
+		cell.style.width = `${this.pixels}px`;
+		cell.style.height = `${this.pixels}px`;
+		cell.style.background = this.color;
+		cell.classList.toggle("grid-cell");
+		return cell;
+	}
+
+	setSelectCellPaint() {
+		const colorDisplay = document.querySelector("#color-display");
+		this.color = hexToRGB(colorDisplay.value);
+		this.element.style.backgroundColor = this.color;
+	}
+
+	setRandomCellPaint() {
+		const red = Math.floor(Math.random() * 256).toString();
+		const green = Math.floor(Math.random() * 256).toString();
+		const blue = Math.floor(Math.random() * 256).toString();
+		this.color = `rgb(${red}, ${green}, ${blue})`;
+		this.element.style.backgroundColor = this.color;
+	}
+
+	eraseCellPaint() {
+		const eraserDisplay = document.querySelector("#eraser-display");
+		this.color = eraserDisplay.value;
+		this.element.style.backgroundColor = this.color;
+	}
+
+	setLightShader() {
+		let rgbValues = this.color.split(",");
+		rgbValues = rgbValues.map(value => {
+			value = +value.replace(/[^0-9.]/g, '');
+			const remainder = 255 - value;
+			return value + remainder * 0.1;
+		});
+		this.color = `rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`;
+		this.element.style.backgroundColor = this.color;
+	}
+
+	setDarkShader() {
+		let rgbValues = this.color.split(",");
+		rgbValues = rgbValues.map(value => {
+			value = +value.replace(/[^0-9.]/g, '');
+			return value * 0.9;
+		});
+		this.color = `rgb(${rgbValues[0]}, ${rgbValues[1]}, ${rgbValues[2]})`;
+		this.element.style.backgroundColor = this.color;
+	}
+}
+
+class Editor {
+	constructor(grid) {
+		this.grid = grid;
+		this.enableColorSelector();
+		this.enableRainbowSelector();
+		this.enableEraserSelector();
+		this.enableLightShaderSelector();
+		this.enableDarkShaderSelector();
+		this.enableScaleSelector();
+	}
+
+	enableColorSelector() {
+		const colorSelector = document.querySelector(".color-selector");
+		const colorDisplay = document.querySelector("#color-display");
+
+		colorSelector.addEventListener("click", () => {
+			colorDisplay.click();
+			this.grid.cellsArray.forEach(cell => {
+				cell.removeColorModes();
+				cell.element.addEventListener("mouseover", cell.setSelectCellPaint);
 			});
 		});
 	}
 
-	colorSelector.addEventListener("click", handleColorSelector);
-	rainbowSelector.addEventListener("click", handleRainbowSelector);
-	eraserSelector.addEventListener("click", handleEraserSelector);
+	enableRainbowSelector() {
+		const rainbowSelector = document.querySelector(".rainbow-selector");
+
+		rainbowSelector.addEventListener("click", () => {
+			this.grid.cellsArray.forEach(cell => {
+				cell.removeColorModes();
+				cell.element.addEventListener("mouseover", cell.setRandomCellPaint);
+			});
+		});
+	}
+
+	enableEraserSelector() {
+		const eraserSelector = document.querySelector(".eraser-selector");
+		const eraserDisplay = document.querySelector("#eraser-display");
+
+		eraserSelector.addEventListener("click", () => {
+			eraserDisplay.click();
+			this.grid.cellsArray.forEach(cell => {
+				cell.removeColorModes();
+				cell.element.addEventListener("click", cell.eraseCellPaint);
+			})
+		});
+	}
+
+	enableLightShaderSelector() {
+		const lightShaderSelector = document.querySelector(".light-shader-selector");
+
+		lightShaderSelector.addEventListener("click", () => {
+			this.grid.cellsArray.forEach(cell => {
+				cell.removeColorModes();
+				cell.element.addEventListener("mouseover", cell.setLightShader);
+			})
+		});
+	}
+
+	enableDarkShaderSelector() {
+		const darkShaderSelector = document.querySelector(".dark-shader-selector");
+		
+		darkShaderSelector.addEventListener("click", () => {
+			this.grid.cellsArray.forEach(cell => {
+				cell.removeColorModes();
+				cell.element.addEventListener("mouseover", cell.setDarkShader);
+			})
+		})
+	}
+	
+	enableScaleSelector() {
+		const scaleSelector = document.querySelector(".scale");
+
+		scaleSelector.addEventListener("change", () => {
+			this.grid.resetGrid();
+			this.grid = new Grid(scaleSelector.value);
+		})
+	}
 }
 
-function setupScaleListener(gridConfig) {
-	const scale = document.querySelector(".scale");
-	scale.addEventListener("change", () => {
-		gridConfig.dimension = scale.value;
-		resetGrid();
-		createGrid(gridConfig);
-	});
+function hexToRGB(hex) {
+	const captures = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	rgb = {
+	  red: parseInt(captures[1], 16),
+	  green: parseInt(captures[2], 16),
+	  blue: parseInt(captures[3], 16)
+	}
+	return `rgb(${rgb.red}, ${rgb.green}, ${rgb.blue})`;
 }
 
-function resetGrid() {
-	const grid = document.querySelector(".grid");
-	while (grid.firstChild) {
-        grid.removeChild(grid.firstChild);
-    }
+setupGame();
+
+function setupGame() {
+	const grid = new Grid(16);
+	const editor = new Editor(grid);
 }
