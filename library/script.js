44ecb316c4069ac0bb7f5c904124f3f1d4c816e7
doanvalerie <valeriephuongdoan@gmail.com>
2023-09-07 00:27:49 -0700

    Define method on Book prototype to add to library

--- library/script.js
+++ library/script.js
@@ -13,6 +13,35 @@ function Book(title, author, pages, description, read) {
 	this.read = read;
 }
 
+Book.prototype.addToLibrary = function() {
+	myLibrary.push(this);
+
+	const card = document.createElement("div");
+	const cardHeader = document.createElement("div");
+	const title = document.createElement("div");
+	const deleteCard = document.createElement("span");
+	const author = document.createElement("div");
+	const pages = document.createElement("div");
+	const description = document.createElement("div");
+	const status = document.createElement("button");
+
+	addHTMLClasses(card, cardHeader, title, deleteCard, author, pages, description, status);
+	addTextToCard(this, title, deleteCard, author, pages, description);		
+	addStylingToStatus(this, status);
+
+	cardHeader.append(title, deleteCard);
+	card.appendChild(cardHeader);
+	card.appendChild(author);
+	card.appendChild(pages);
+	card.appendChild(description);
+	card.appendChild(status);
+	library.appendChild(card);
+
+	card.dataset.index = myLibrary.length - 1;
+	addStatusChangerListener(status);
+	addCardRemovalListener(deleteCard);
+}
+
 function addDialogListeners() {
 	// Display dialog when button is clicked
 	addBook.addEventListener("click", () => dialog.showModal());
@@ -72,41 +101,10 @@ function addFormSubmissionListener() {
 			const read = form.elements["status"].value === "read" ? true : false;
 
 			const newBook = new Book(title, author, pages, description, read);
-			addBookToLibrary(newBook);
+			newBook.addToLibrary();
 			form.reset();
 		}
 	});
-}
-
-function addBookToLibrary(...books) {
-	for (let book of books) {
-		myLibrary.push(book);
-
-		const card = document.createElement("div");
-		const cardHeader = document.createElement("div");
-		const title = document.createElement("div");
-		const deleteCard = document.createElement("span");
-		const author = document.createElement("div");
-		const pages = document.createElement("div");
-		const description = document.createElement("div");
-		const status = document.createElement("button");
-
-		addHTMLClasses(card, cardHeader, title, deleteCard, author, pages, description, status);
-		addTextToCard(book, title, deleteCard, author, pages, description);		
-		addStylingToStatus(book, status);
-
-		cardHeader.append(title, deleteCard);
-		card.appendChild(cardHeader);
-		card.appendChild(author);
-		card.appendChild(pages);
-		card.appendChild(description);
-		card.appendChild(status);
-		library.appendChild(card);
-
-		card.dataset.index = myLibrary.length - 1;
-		addStatusChangerListener(status);
-		addCardRemovalListener(deleteCard);
-	}
 }
 
 function addHTMLClasses(card, cardHeader, title, deleteCard, author, pages, description, status) {
@@ -174,7 +172,8 @@ const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "Lorem ipsum dol
 const animalFarm = new Book("Animal Farm", "George Orwell", 140, "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut modi molestias cupiditate ratione! Laboriosam exercitationem quas reiciendis, quam, nesciunt quidem inventore quo iusto, repellendus ut voluptates perspiciatis minima esse molestiae.", true);
 const handmaidsTale = new Book("The Handmaid's Tale", "Margaret Atwood", 311, "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolorem alias nostrum, eius est temporibus unde magnam maxime aspernatur ex voluptate in porro consectetur, nemo odit deleniti asperiores. Expedita, veniam.", false);
 
-addBookToLibrary(theHobbit, animalFarm, handmaidsTale); 
+defaultBooks = [theHobbit, animalFarm, handmaidsTale];
+defaultBooks.forEach(book => book.addToLibrary());
 addDialogListeners();
 addInputListeners();
 addFormSubmissionListener();
