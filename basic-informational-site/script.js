b4cdcbb433282c5042446aacdc57a10c10b153c3
doanvalerie <valeriephuongdoan@gmail.com>
2023-12-23 16:00:40 -0800

    Initial commit of basic informational site

---
+++ basic-informational-site/script.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const fs = require("fs");
+
+const hostname = "localhost";
+const port = 8080;
+
+function serveErrorPage(res) {
+  fs.readFile("./pages/404.html", "utf-8", (err, data) => {
+    if (!err) {
+      res.statusCode = 404;
+      res.end(data);
+    }
+  });
+}
+
+function serveStaticPage(data, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end(data);
+}
+
+const server = http.createServer((req, res) => {
+  const pathname = req.url;
+  let html;
+
+  if (pathname == "/") {
+    html = "./pages/index.html";
+  } else if (pathname !== "/index") {
+    html = `./pages${pathname}.html`;
+  }
+
+  fs.readFile(html, "utf-8", (err, data) => {
+    if (err) {
+      serveErrorPage(res);
+    } else {
+      serveStaticPage(data, res);
+    }
+  });
+});
+
+server.listen(port, hostname);
